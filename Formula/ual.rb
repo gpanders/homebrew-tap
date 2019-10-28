c5e3d1d040c030b7468a9a8af879c78c494a9ce4
Greg Anders <greg@gpanders.com>
2019-10-27 21:11:39 -0600

    Update ual description

--- Formula/ual.rb
+++ Formula/ual.rb
@@ -1,5 +1,5 @@
 class Ual < Formula
-  desc "Keep your filesystem free of clutter"
+  desc "Create and manage your own man pages"
   homepage "https://github.com/gpanders/ual"
   url "https://github.com/gpanders/ual/archive/v0.1.0.tar.gz"
   sha256 "a596c8921f1010e4f0d3b7e354d4923f05f6e0282abe4523ec2fa5d43c5a9f1b"
