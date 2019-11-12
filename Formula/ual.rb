bffa1fa43e93e19edca6f930a72fb979064c6517
Greg Anders <greg@gpanders.com>
2019-11-12 08:02:09 -0700

    ual 0.1.1

--- Formula/ual.rb
+++ Formula/ual.rb
@@ -1,8 +1,8 @@
 class Ual < Formula
   desc "Create and manage your own man pages"
   homepage "https://github.com/gpanders/ual"
-  url "https://github.com/gpanders/ual/archive/v0.1.0.tar.gz"
-  sha256 "a596c8921f1010e4f0d3b7e354d4923f05f6e0282abe4523ec2fa5d43c5a9f1b"
+  url "https://github.com/gpanders/ual/archive/v0.1.1.tar.gz"
+  sha256 "c75dca0719d97b2467200902cbb99645fc9353c5fadbefee8b0437f10ac238de"
   head "https://github.com/gpanders/ual.git"
 
   def install
