4e4de7a073146360a75f75ff0e1e0c9cfc0f90b2
Greg Anders <greg@gpanders.com>
2019-11-25 18:08:30 -0700

    ual v0.2.0

--- Formula/ual.rb
+++ Formula/ual.rb
@@ -1,16 +1,17 @@
 class Ual < Formula
   desc "Create and manage your own man pages"
   homepage "https://github.com/gpanders/ual"
-  url "https://github.com/gpanders/ual/archive/v0.1.2.tar.gz"
-  sha256 "7628a4edd807b432f9c21cf3e3e57c61d3b99323a1ad37ed84ed5d13ed58d2d5"
+  url "https://github.com/gpanders/ual/archive/v0.2.0.tar.gz"
+  sha256 "de42c369e48c59db562ad1b596e81fec2e7661ac809945964e20b1f791372db8"
   head "https://github.com/gpanders/ual.git"
+
+  depends_on "pandoc"
 
   def install
     bin.install "ual"
     bash_completion.install "completions/bash/ual"
     fish_completion.install "completions/fish/ual.fish"
     zsh_completion.install "completions/zsh/_ual"
-
   end
 
   test do
