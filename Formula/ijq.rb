38272ad9826e750a0829e17bd3ecfe075df75bb8
Greg Anders <8965202+gpanders@users.noreply.github.com>
2020-05-26 18:09:54 -0600

    ijq: add tagged release

--- Formula/ijq.rb
+++ Formula/ijq.rb
@@ -1,6 +1,8 @@
 class Ijq < Formula
   desc "Interactive jq"
   homepage "https://git.sr.ht/~gpanders/ijq"
+  url "https://git.sr.ht/~gpanders/ijq/archive/v0.1.0.tar.gz"
+  sha256 "6e01bcea5674269c8673bedcfdf14adde77e82f313fe6112f991306bd233c47b"
   head "https://git.sr.ht/~gpanders/ijq", :using => :git
 
   depends_on "go" => :build
