f9e0fd6194ef8718bf89367f36eb0f305617ecf0
Chris Koehnke <chris.koehnke@elastic.co>
2020-08-10 14:14:22 -0400

    ijq: update checksum

--- Formula/ijq.rb
+++ Formula/ijq.rb
@@ -2,7 +2,7 @@ class Ijq < Formula
   desc "Interactive jq"
   homepage "https://git.sr.ht/~gpanders/ijq"
   url "https://git.sr.ht/~gpanders/ijq/archive/v0.1.0.tar.gz"
-  sha256 "6e01bcea5674269c8673bedcfdf14adde77e82f313fe6112f991306bd233c47b"
+  sha256 "59f1b3b612e92f3998890edc25ee2109453b09e54d763c0b8153590cd47d657e"
   head "https://git.sr.ht/~gpanders/ijq", :using => :git
 
   depends_on "go" => :build
