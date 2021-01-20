4438477dc32cd7c89aa7f008f9c70c1d43781bfc
Gregory Anders <greg@gpanders.com>
2021-01-19 17:10:10 -0700

    ijq 0.2.2

--- Formula/ijq.rb
+++ Formula/ijq.rb
@@ -1,8 +1,8 @@
 class Ijq < Formula
   desc "Interactive jq"
   homepage "https://git.sr.ht/~gpanders/ijq"
-  url "https://git.sr.ht/~gpanders/ijq/archive/v0.2.1.tar.gz"
-  sha256 "33c1d0477fae17d4fbd6b8fa246622be9e0cd7129e39f3e939ef7e1830917a31"
+  url "https://git.sr.ht/~gpanders/ijq/archive/v0.2.2.tar.gz"
+  sha256 "e2a1898f04554880155a8421438169f56f8e99014ff98a81d732aca3e9a8ce7d"
   head "https://git.sr.ht/~gpanders/ijq", :using => :git
 
   depends_on "go" => :build
